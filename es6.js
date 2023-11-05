cdb3850ea043492bfbe40f7860332015ca583a47
chojeongeun <juy0816@naver.com>
2023-11-05 11:48:29 +0900

    인수를 깜빡하고 못 넣었을 때 방지하는 default parameter

--- es6.js
+++ es6.js
@@ -1,5 +1,6 @@
 class FontStyle {
-	constructor(el, size, color) {
+	//파라미터를 안넣었을 때 default parameter 지정할 수도 있음
+	constructor(el, size, color = 'hotpink') {
 		this.el = document.querySelector(el);
 		this.changeSize(size);
 		this.changeColor(color);
@@ -13,4 +14,4 @@ class FontStyle {
 }
 
 new FontStyle('#tit1', '100px', 'pink');
-new FontStyle('#tit2', '50px', 'lightgreen');
+new FontStyle('#tit2', '50px');
